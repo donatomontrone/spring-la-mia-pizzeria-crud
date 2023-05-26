1ed732a1ed0a8ca69c72b57b4b39dcc9298392f8
donatomontrone <donato.montrone@outlook.com>
2023-05-26 14:29:31 +0200

    setup repo

---
+++ src/main/java/org/java/demo/pizzeria/SpringLaMiaPizzeriaCrudApplication.java
@@ -0,0 +1,20 @@
+package org.java.demo.pizzeria;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
+
+}
